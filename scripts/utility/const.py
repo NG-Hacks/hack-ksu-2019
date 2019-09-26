cc3ac540cf5ade1102aa2ac7802a5f1d101ed9d5
ngngardner <ngngardner@gmail.com>
2019-09-26 09:27:27 -0400

    app and context

--- scripts/utility/const.py
+++ scripts/utility/const.py
@@ -1,3 +1,21 @@
 
+# base config data
 API_KEY = 'API_KEY'
 BASE_URL = 'BASE_URL'
+
+# api headers
+CONTENT_TYPE = 'Content-Type'
+X_API_KEY = 'X-API-Key'
+
+# general
+HEADERS = 'HEADERS'
+
+# endpoint data
+ACCOUNT_ID = 'accountId'
+COUNTER_PARTY = 'counterParty'
+TYPE = 'type'
+DESCRIPTION = 'description'
+AMOUNT = 'amount'
+DATE = 'date'
+STATUS = 'status'
+TEXT = 'text'
