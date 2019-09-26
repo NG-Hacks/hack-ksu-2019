7cc807f86ac3346c177c72c2c9b5b52864e3385f
ngngardner <ngngardner@gmail.com>
2019-09-26 09:25:41 -0400

    endpoints initialization

---
+++ scripts/endpoints/__init__.py
@@ -0,0 +1,21 @@
+
+class Endpoints:
+
+    def initialize():
+        # accounts
+        '''
+        from .accounts.accounts_by_owner import accounts_by_owner
+        from .accounts.create_account import create_account
+        from .accounts.inspect_account import inspect_account
+        from .accounts.list_accounts import list_accounts
+        from .accounts.update_balance import update_balance
+        from .accounts.update_owner import update_owner
+        '''
+
+        # transactions
+        '''
+        from .transactions.delete_transaction import delete_transaction
+        from .transactions.inspect_transaction import inspect_transaction
+        from .transactions.list_transactions import list_transactions
+        '''
+        from .transactions.post_transaction import post_transaction
---
+++ scripts/endpoints/transactions/post_transaction.py
@@ -0,0 +1,57 @@
+
+# standard
+from datetime import datetime
+import time
+import logging
+import json
+
+# packages
+import requests
+
+# internal
+from scripts.context.context import Context
+from scripts.utility import const
+
+_LOGGER = logging.getLogger(__name__)
+
+BASE_URL = Context.data()[const.BASE_URL]
+HEADERS = Context.data()[const.HEADERS]
+
+def post_transaction(
+    accountID:str,
+    counterParty:str,
+    transactionType:str,
+    description:str,
+    amount:int,
+    date:datetime=None):
+    '''
+        This function interacts with the API Endpoint to create
+        a transaction on the specified account.
+    '''
+
+    req_url = BASE_URL + f'/{accountID}/transactions'
+
+    data = {
+        const.COUNTER_PARTY : counterParty,
+        const.TYPE : transactionType,
+        const.DESC : description,
+        const.AMOUNT : amount,
+    }
+
+    # if a date is not specified, the api will fill
+    # in the date with the current time
+    if date:
+        data[const.DATE] = date
+
+    resp = requests.post(
+        url = req_url,
+        headers = HEADERS,
+        data = json.dumps(data)
+    )
+
+    res = {
+        const.STATUS : resp.status_code,
+        const.TEXT : resp.text
+    }
+
+    return res
